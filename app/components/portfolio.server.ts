050fc702ed8de5461a1a9727583d87868fdb6330
bhavish <bhavishp@ivoyant.com>
2024-06-05 14:14:51 +0530

    form validation without posting destination

--- app/components/portfolio.server.ts
+++ app/components/portfolio.server.ts
@@ -3,7 +3,6 @@ import PocketBase from 'pocketbase';
 const pb = new PocketBase(process.env.POCKETBASE_BASE_URL);
 export default pb;
 
-
 export const auth = async()=>{
     let authData = await pb.admins.authWithPassword(process.env.POCKETBASE_EMAIL as string, process.env.POCKETBASE_PASS as string);
     return authData;
