8e3fc93ef1eb1eefca337470f2cf9d9f9ff32a14
bhavish <bhavishp@ivoyant.com>
2024-06-20 11:27:27 +0530

    Css background clip text was affecting the emoji's so I've fixed it

--- app/routes/_main.blog_.$slug.tsx
+++ app/routes/_main.blog_.$slug.tsx
@@ -33,7 +33,8 @@ export const meta: MetaFunction = ({ data }: { data:any }) => {
 }
 export default function BlogContent() {
     const data = useLoaderData<typeof loader>()
-
+    const EMOJI_REGEX = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+    const title = data.blog.title.replace(EMOJI_REGEX, `<span>$&</span>`)
     useEffect(() => {
         document.querySelectorAll('pre code').forEach((block) => {
             hljs.highlightBlock(block as HTMLElement);
@@ -45,7 +46,7 @@ export default function BlogContent() {
                 <div className="hero p-4 text-center py-20 md:py-40">
                     <h1 className="inline-block font-font-serif text-xl font-extrabold md:text-4xl">
                         <div className="font-font-monospace text-base font-mono font-normal md:text-2xl">{convertDateString(data.blog.updated)}</div>
-                        <div className="py-2 bg-gradient-to-r from-orange-600 via-red-500 to-orange-400 text-transparent bg-clip-text px-3 text-4xl md:text-7xl">{data.blog.title}</div>
+                        <div dangerouslySetInnerHTML={{ __html: `<h1>${ title }</h1>` }} className="py-2 custom-gradient-text px-3 text-4xl md:text-7xl"/>
                     </h1>
                 </div>
             </section>
@@ -54,10 +55,10 @@ export default function BlogContent() {
                 src={`${ENV.BASE_URL}/api/files/${data.blog.collectionId}/${data.blog.id}/${data.blog.heroImage}`}
                 alt="Hero Image for this blog" />
             <div className="my-8 md:my-12 m-auto w-full max-w-4xl p-4">
-                <h2 className="bg-gradient-to-r from-orange-600 via-red-500 to-orange-400 text-transparent bg-clip-text m-0 mb-2 inline-block text-left text-3xl md:text-4xl">
-                    {data.blog.title}
+                <h2 dangerouslySetInnerHTML={{ __html: `<h1>${ title }</h1>` }} className="custom-gradient-text m-0 mb-2 inline-block text-left text-3xl md:text-4xl">
+                    
                 </h2>
-                <div className="font-font-monospace text-sm">
+                <div className="font-monospace text-sm">
                     {convertDateString(data.blog.updated)}
                 </div>
             </div>
